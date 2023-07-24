e61a281075dd83a94c009de2a47175606da84315
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-24 21:13:13 +0900

    Add SearchView

--- app/src/main/java/com/example/trialqiitaclientapp/view/component/SearchView.kt
+++ app/src/main/java/com/example/trialqiitaclientapp/view/component/SearchView.kt
@@ -1,8 +1,71 @@
 package com.example.trialqiitaclientapp.view.component
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchView() {
+fun SearchView(
+    textFieldState: MutableState<TextFieldValue>,
+    onSubmit: (text: String) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
 
+    TextField(
+        value = textFieldState.value,
+        onValueChange = { value ->
+            textFieldState.value = value
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "searchIcon",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (textFieldState.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        textFieldState.value = TextFieldValue("")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "delete icon",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions {
+            focusManager.clearFocus()
+            onSubmit(textFieldState.value.text)
+        }
+    )
 }
