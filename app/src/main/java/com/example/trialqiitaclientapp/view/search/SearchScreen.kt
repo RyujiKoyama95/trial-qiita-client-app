b8f6c5f616a792159334a9bbe36967f954933dd5
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-25 09:13:03 +0900

    Add SearchScreen

--- app/src/main/java/com/example/trialqiitaclientapp/view/search/SearchScreen.kt
+++ app/src/main/java/com/example/trialqiitaclientapp/view/search/SearchScreen.kt
@@ -1,8 +1,27 @@
 package com.example.trialqiitaclientapp.view.search
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.navigation.NavController
+import com.example.trialqiitaclientapp.view.component.SearchBar
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun SearchScreen() {
-
+fun SearchScreen(navController: NavController, vm: SearchViewModel) {
+    Scaffold {
+        Column {
+            val textFieldState = remember { mutableStateOf(TextFieldValue("")) }
+            SearchBar(textFieldState = textFieldState) {
+                // Todo: query url
+                vm.searchArticle("")
+            }
+        }
+    }
 }
