103570298d5228893c1210fcc290dab067e0c2eb
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-27 09:19:13 +0900

    Add webView to DetailScreen

--- app/src/main/java/com/example/trialqiitaclientapp/view/detail/DetailScreen.kt
+++ app/src/main/java/com/example/trialqiitaclientapp/view/detail/DetailScreen.kt
@@ -1,8 +1,17 @@
 package com.example.trialqiitaclientapp.view.detail
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun DetailScreen() {
-
+fun DetailScreen(url: String) {
+    AndroidView(
+        factory = ::WebView,
+        update = { webView ->
+            webView.webViewClient = WebViewClient()
+            webView.loadUrl(url)
+        }
+    )
 }
