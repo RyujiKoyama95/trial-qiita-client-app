3ca7776420d6597b231b87db2059d431b69d90b8
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-29 07:11:01 +0900

    Remove import

--- app/src/main/java/com/example/trialqiitaclientapp/MainActivity.kt
+++ app/src/main/java/com/example/trialqiitaclientapp/MainActivity.kt
@@ -6,14 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
